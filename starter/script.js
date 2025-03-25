3260431798347af1c5d510e749e2236fcce61ecd
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2025-03-25 12:26:15 +0100

    first commit

--- starter/script.js
+++ starter/script.js
@@ -5,3 +5,9 @@ console.log(40 + 8 + 23 - 10);
 
 let firstname = "jonas";
 console.log(firstname);
+
+const ageJohnas = 2037 - 1991;
+const ageSare = 2037 - 2018;
+console.log(ageSare, ageJohnas);
+
+console.log(2 * 2);
