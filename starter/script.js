4efa85db1155062d030b82cb7f5a0d5083211798
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2025-03-25 14:24:06 +0100

    still on

--- starter/script.js
+++ starter/script.js
@@ -1,13 +1,75 @@
 let js = "amazing";
 if (js === "amazing") alert("javascript is fun!");
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
-let firstname = "jonas";
-console.log(firstname);
+// let firstname = "jonas";
+// console.log(firstname);
 
-const ageJohnas = 2037 - 1991;
-const ageSare = 2037 - 2018;
-console.log(ageSare, ageJohnas);
+// const ageJohnas = 2037 - 1991;
+// const ageSare = 2037 - 2018;
 
-console.log(2 * 2);
+// console.log(2 ** 3);
+// const firstName = "ageJohnas";
+// const lastName = "agechichi";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// console.log(x);
+// const averageAge = (ageJohnas + ageSare) / 2;
+// console.log(ageSare, ageJohnas, averageAge);
+
+// // code challage
+// //Test one
+// const marksFirstWeight = 78;
+// const marksFirstHeight = 1.69;
+// const marksFirstBMI = marksFirstWeight / (marksFirstHeight * marksFirstHeight);
+// // john
+// const johnsFirstWeight = 92;
+// const johnsFirstHeight = 1.95;
+// const johnsFirstBMI = johnsFirstWeight / (johnsFirstHeight * johnsFirstHeight);
+// console.log(marksFirstBMI, johnsFirstBMI);
+// // the test1
+// const markHigherBMI = marksFirstBMI > johnsFirstBMI;
+// console.log(markHigherBMI);
+// // test 2
+// const markSecondweight = 95;
+// const markSecondHeight = 1.88;
+// const markSecondBMI = markSecondweight / (markSecondHeight * markSecondHeight);
+// // john
+// const johnsSecondWeight = 85;
+// const johnsSecondHeight = 1.76;
+// const johnsSecondBMI =
+//   johnsSecondWeight / (johnsSecondHeight * johnsSecondHeight);
+
+// // the test 2
+// console.log(markSecondBMI, johnsSecondBMI);
+// const markHigherBMITwo = markSecondBMI > johnsSecondBMI;
+// console.log(markHigherBMITwo);
+
+// strings
+// const firstName = "jonas";
+// const job = "teacher";
+// const birthyear = 1991;
+// const year = 2037;
+
+// const jonas =
+//   "i'm " + firstName + " a " + (year - birthyear) + " years old " + job + "!S";
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName} a ${year - birthyear} year old ${job}`;
+// console.log(jonasNew);
+// console.log(`hello
+// hiii
+//  whasup`);
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log("sara can start driving leasons 👌");
+} else {
+  const yearsleft = 18 - age;
+  console.log(`sara is too younge, wait ${yearsleft} years`);
+}
