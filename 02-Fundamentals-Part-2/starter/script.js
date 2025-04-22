8fd2e8dc05b45a25ec7b35eca866943429f1ca72
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2025-04-22 15:58:44 +0100

    decleartion

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -18,9 +18,24 @@ function logger() {
 logger();
 
 function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `juice with ${apples} apples and ${oranges}oranges.`;
+  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
 const applejuice = fruitProcessor(5, 0);
 console.log(applejuice);
+
+const appleOrangejuice = fruitProcessor(2, 4);
+console.log(appleOrangejuice);
+
+function calcage1(birthyear) {
+  return 2037 - birthyear;
+}
+
+const age = calcage1(1998);
+console.log(age);
+
+const calAge2 = function (birthyear) {
+  return 2037 - birthyear;
+};
+const age2 = calAge2(1998);
+console.log(age2);
